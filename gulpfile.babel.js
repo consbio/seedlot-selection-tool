489dc0b04f72e899937fd7c68a088b80646543da
Nik Molnar <nik.molnar@consbio.org>
2017-10-24 16:32:24 -0700

    Add `--production` option to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,49 @@
 import gulp from 'gulp'
 import sass from 'gulp-sass'
-import envify from 'envify/custom'
-import gulpUtil from 'gulp-util'
+import util from 'gulp-util'
+import rename from 'gulp-rename'
 import browserify from 'browserify'
 import tildeImporter from 'node-sass-tilde-importer'
 import source from 'vinyl-source-stream'
 
+const production = !!util.env.production
+
 gulp.task('sass', () => {
     return gulp.src('./sass/seedsource.scss')
-        .pipe(sass({importer: tildeImporter}).on('error', sass.logError))
+        .pipe(
+            sass({importer: tildeImporter, outputStyle: production ? 'compressed' : 'expanded'})
+                .on('error', sass.logError)
+        )
+        .pipe(rename(production ? 'seedsource.min.css' : 'seedsource.css'))
         .pipe(gulp.dest('./source/sst/static/css'))
 })
 
 gulp.task('js', () => {
-    browserify({
+    let transform = []
+    if (production) {
+        transform = [
+            ['envify', {NODE_ENV: 'production', global: true}]
+        ]
+    }
+
+    let b = browserify({
         entries: './seedsource-core/js/index.js',
         paths: ['./js', './seedsource-core/js'],
         extensions: ['.jsx'],
-        transform: [
-            // ['envify', {NODE_ENV: 'production', global: true}]
-        ]
+        transform
     })
         .transform('babelify', {presets: ['es2015', 'react']})
-        // .transform('uglifyify', {global: true})
-        .bundle()
-        .on('error', gulpUtil.log)
-        .pipe(source('seedsource.js'))
+
+    if (production) {
+        b = b.transform('uglifyify', {global: true})
+    }
+
+    b = b.bundle()
+        .on('error', util.log)
+        .pipe(source(production ? 'seedsource.min.js' : 'seedsource.js'))
         .pipe(gulp.dest('./source/sst/static/js'))
+
+    return b
 })
 
 gulp.task('default', ['sass', 'js'])
