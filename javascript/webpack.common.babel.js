2f3d0f0b43dabbf85813e9d852cc1663786910b2
Nik Molnar <nik.molnar@consbio.org>
2018-06-18 19:14:40 -0700

    Fix images path

--- javascript/webpack.common.babel.js
+++ javascript/webpack.common.babel.js
@@ -31,7 +31,7 @@ export default {
                 test: /\.(png|pdf|gif|jpe?g|svg)$/,
                 loader: 'file-loader',
                 options: {
-                    name: '[path][name].[hash].[ext]'
+                    name: '/static/[path][name].[hash].[ext]'
                 }
             }
         ]
