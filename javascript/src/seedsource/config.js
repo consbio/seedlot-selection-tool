908931ad61b90e7f5db35781b63d8c629150851b
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2018-08-27 16:39:09 -0700

    Add species range constraint

--- javascript/src/seedsource/config.js
+++ javascript/src/seedsource/config.js
@@ -1,5 +1,37 @@
+import baseConfig from 'core/seedsource/config'
+
 const config = {
-    apiRoot: '/sst/'
+    apiRoot: '/sst/',
+    species: [
+        {
+            name: 'psme',
+            label: 'Douglas-fir'
+        },
+        {
+            name: 'pico',
+            label: 'Lodgepole pine'
+        },
+        {
+            name: 'piba',
+            label: 'Jack pine'
+        },
+        {
+            name: 'pipo',
+            label: 'Ponderosa pine'
+        },
+        {
+            name: 'pima',
+            label: 'Black spruce'
+        },
+        {
+            name: 'thpl',
+            label: 'Western red cedar'
+        },
+        {
+            name: 'pimo',
+            label: 'Western white pine'
+        }
+    ]
 }
 
-export default Object.assign(config, window.SS_CONFIG)
+export default Object.assign(baseConfig, config)
