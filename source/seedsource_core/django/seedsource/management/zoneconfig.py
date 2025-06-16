8411b1624e869171f1f42b8c154feed669682b73
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2025-06-16 14:39:33 -0700

    Allow for skipping import of some zones

--- source/seedsource_core/django/seedsource/management/zoneconfig.py
+++ source/seedsource_core/django/seedsource/management/zoneconfig.py
@@ -96,7 +96,9 @@ class ZoneConfig:
 
                     info = self.config.get_zone_info(feature, file)
 
-                    if info is not None:
+                    if info == {}:
+                        continue
+                    elif info is not None:
                         yield polygon, info
 
                     else:
