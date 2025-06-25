9e4e51fee775079328ecf5e5b5f11e04d054bac6
Jesudas Joseph <jess.joseph@consbio.org>
2025-06-25 10:43:53 -0700

    Add announcement modal and extract messages

---
+++ javascript/src/seedsource-ui/components/AnnouncementModal.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { t } from 'ttag'
+import ModalCard from './ModalCard'
+
+type AnnouncementModalProps = {
+  announcementID: string
+  title: string
+  onClose: () => void
+}
+
+class AnnouncementModal extends React.Component<AnnouncementModalProps> {
+  modal?: ModalCard | null
+  noShowCheckbox?: HTMLInputElement | null
+
+  show() {
+    if (!localStorage.getItem(this.props.announcementID)) {
+      this.modal?.show()
+    }
+
+    setTimeout(() => {
+      this.noShowCheckbox?.focus()
+    }, 1)
+  }
+
+  hide() {
+    this.modal?.hide()
+  }
+
+  accept = () => {
+    if (this.noShowCheckbox?.checked) {
+      localStorage.setItem(this.props.announcementID, 'true')
+    }
+    this.props.onClose()
+    this.modal?.hide()
+  }
+
+  render() {
+    const { title, children } = this.props
+    return (
+      <ModalCard
+        ref={input => {
+          this.modal = input
+        }}
+        title={title}
+      >
+        {children}
+        <div className="field">
+          <div className="control">
+            <input
+              type="checkbox"
+              id="dontshow"
+              className="is-checkradio is-info"
+              ref={input => {
+                this.noShowCheckbox = input
+              }}
+            />
+            <label htmlFor="dontshow">{t`Don't show again`}</label>
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <button type="button" className={`button is-primary`} onClick={this.accept}>
+              {t`Ok`}
+            </button>
+          </div>
+        </div>
+      </ModalCard>
+    )
+  }
+}
+
+export default AnnouncementModal
