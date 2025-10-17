2754d2d4395ff538b4ec43035366760c7e5925ba
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2025-10-17 14:57:17 -0700

    Fix GeoTIFF download

--- source/seedsource_core/django/seedsource/tasks/write_tif.py
+++ source/seedsource_core/django/seedsource/tasks/write_tif.py
@@ -1,11 +1,10 @@
+import secrets
 import zipfile
-from stat import S_IRUSR, S_IRGRP, S_IWUSR
 
 import netCDF4
 import numpy as np
 import os.path
 import rasterio
-import tempfile
 from django.conf import settings
 from io import BytesIO
 
@@ -18,12 +17,12 @@ from rasterio.transform import Affine
 
 
 class WriteTIF(Task):
-    name = 'write_tif'
+    name = "write_tif"
     inputs = [
-        StringParameter('service_id'),
-        StringParameter('language_code', required=False)
+        StringParameter("service_id"),
+        StringParameter("language_code", required=False),
     ]
-    outputs = [StringParameter('filename')]
+    outputs = [StringParameter("filename")]
 
     def execute(self, service_id, language_code=None):
         if language_code is None:
@@ -33,41 +32,60 @@ class WriteTIF(Task):
         svc = Service.objects.get(name=service_id)
         var = Variable.objects.get(service_id=svc.id)
         data_path = svc.data_path
-        with netCDF4.Dataset(os.path.join(SERVICE_DATA_ROOT, data_path), 'r') as nc:
-            data = nc.variables[var.name][:].astype('uint8')
+        with netCDF4.Dataset(os.path.join(SERVICE_DATA_ROOT, data_path), "r") as nc:
+            data = nc.variables[var.name][:].astype("uint8")
+            y_data = nc.variables[var.y_dimension]
+            y_increasing = y_data[0] < y_data[1]
 
         height, width = data.shape
         ex = var.full_extent
         x_step = (ex.xmax - ex.xmin) / width
         y_step = (ex.ymax - ex.ymin) / height
-        transform = Affine.from_gdal(ex.xmin, x_step, 0, ex.ymax, 0, -y_step)
+        transform = Affine.from_gdal(
+            ex.xmin,
+            x_step,
+            0,
+            ex.ymin if y_increasing else ex.ymax,
+            0,
+            y_step if y_increasing else -y_step,
+        )
         dtype = np.uint8
         nodata = 128
 
         if not settings.DATASET_DOWNLOAD_DIR.exists():
             settings.DATASET_DOWNLOAD_DIR.mkdir()
 
-        fd, filename = tempfile.mkstemp(dir=str(settings.DATASET_DOWNLOAD_DIR), suffix='.zip')
-        os.close(fd)
-        os.chmod(filename, S_IRUSR | S_IWUSR | S_IRGRP)
+        filename = settings.DATASET_DOWNLOAD_DIR / f"tmp{secrets.token_urlsafe(5)}.zip"
 
-        with zipfile.ZipFile(filename, mode='w') as zf:
+        with zipfile.ZipFile(filename, mode="w") as zf:
             tif_data = BytesIO()
 
             with rasterio.Env(GDAL_TIFF_INTERNAL_MASK=True):
                 opts = dict(
-                    driver='GTiff', height=height, width=width, crs=var.projection, transform=transform, count=1,
-                    dtype=dtype, nodata=nodata
+                    driver="GTiff",
+                    height=height,
+                    width=width,
+                    crs=var.projection,
+                    transform=transform,
+                    count=1,
+                    dtype=dtype,
+                    nodata=nodata,
                 )
-                with rasterio.open(tif_data, 'w', **opts) as dst:
+                with rasterio.open(tif_data, "w", **opts) as dst:
                     dst.write(np.array(data, dtype=dtype), 1)
                     dst.write_mask(np.logical_not(data.mask))
 
-                zf.writestr('SST Results/results.tif', tif_data.getvalue(), compress_type=zipfile.ZIP_DEFLATED)
                 zf.writestr(
-                    'SST Results/README.txt',
-                    render_to_string('txt/download_readme.txt', {'language_code': language_code}),
-                    compress_type=zipfile.ZIP_DEFLATED
+                    "SST Results/results.tif",
+                    tif_data.getvalue(),
+                    compress_type=zipfile.ZIP_DEFLATED,
+                )
+                zf.writestr(
+                    "SST Results/README.txt",
+                    render_to_string(
+                        "txt/download_readme.txt", {"language_code": language_code}
+                    ),
+                    compress_type=zipfile.ZIP_DEFLATED,
                 )
 
         return os.path.basename(filename)
